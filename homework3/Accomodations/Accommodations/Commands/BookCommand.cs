2862f4cd4397c761c7e1f83631ea19eab97697e4
Андрей Пряшников <prandruha2003@yandex.ru>
2024-04-08 17:01:01 +0300

    Init debug

---
+++ homework3/Accomodations/Accommodations/Commands/BookCommand.cs
@@ -0,0 +1,36 @@
+using Accommodations.Dto;
+using Accommodations.Models;
+
+namespace Accommodations.Commands;
+
+public class BookCommand(IBookingService bookingService, BookingDto bookingDto) : ICommand
+{
+    private Booking? _executedBookingDto;
+
+    public void Execute()
+    {
+        //Check in date
+        if (bookingDto.StartDate.Date < DateTime.Today)
+        {
+            throw new ArgumentException("Start date cannot be earlier than now date");
+        }
+        Currency currency = bookingDto.Currency switch
+        {
+            CurrencyDto.Usd => Currency.Usd,
+            CurrencyDto.Rub => Currency.Rub,
+            CurrencyDto.Cny => Currency.Cny,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        _executedBookingDto = bookingService.Book(bookingDto.UserId, bookingDto.Category, bookingDto.StartDate,
+            bookingDto.EndDate, currency);
+        Console.WriteLine($"Booking successful: ID {_executedBookingDto!.Id}");
+    }
+
+    public void Undo()
+    {
+        bookingService.CancelBooking(_executedBookingDto.Id);
+        decimal cancellationPenalty = bookingService.CalculateCancellationPenaltyAmount(_executedBookingDto);
+        Console.WriteLine(
+            $"Booking {_executedBookingDto.Id} was canceled. Cancellation penalty: {cancellationPenalty}");
+    }
+}
