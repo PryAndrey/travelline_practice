62c9dd9e18d1437ebd141a52f0ec5038bef84afa
Андрей Пряшников <prandruha2003@yandex.ru>
2024-05-12 21:06:46 +0300

    Изменения по замечаниям(lw4)

--- homework4/Homework4_SELECTIONs.sql
+++ homework4/Homework4_SELECTIONs.sql
@@ -1,28 +1,38 @@
-SELECT rooms.* FROM rooms
-	JOIN bookings ON bookings.room_id = rooms.room_id
-WHERE (GETDATE() NOT BETWEEN bookings.check_in_date AND bookings.check_out_date ) AND availability = 1
+SELECT rooms.*
+FROM rooms
+         JOIN bookings ON bookings.room_id = rooms.room_id
+WHERE (GETDATE() NOT BETWEEN bookings.check_in_date AND bookings.check_out_date)
+  AND availability = 1
 
-SELECT * FROM customers
+SELECT *
+FROM customers
 WHERE last_name LIKE 'S%';
 
-SELECT bookings.* FROM bookings
-	JOIN customers ON bookings.customer_id = customers.customer_id
-WHERE customers.first_name = N'John' OR customers.email = N'johndoe@gmail.com';
+SELECT bookings.*
+FROM bookings
+         JOIN customers ON bookings.customer_id = customers.customer_id
+WHERE customers.first_name = N'John'
+   OR customers.email = N'johndoe@gmail.com';
 
-SELECT bookings.* FROM bookings
-	JOIN rooms ON bookings.customer_id = rooms.room_id
+SELECT bookings.*
+FROM bookings
+         JOIN rooms ON bookings.customer_id = rooms.room_id
 WHERE rooms.room_number = 64;
 
-SELECT rooms.* FROM rooms
-WHERE room_id IN 
-	(SELECT room_id FROM bookings
-	 WHERE rooms.[availability] = 1 
-		AND '2024-05-15' NOT BETWEEN bookings.check_in_date AND bookings.check_out_date);
+--Находим номера не забронированные на определенную дату
+--(Дата не находится в диапазоне бронирования номера)
+SELECT rooms.*
+FROM rooms
+WHERE room_id IN
+      (SELECT room_id
+       FROM bookings
+       WHERE '2024-05-15' NOT BETWEEN bookings.check_in_date AND bookings.check_out_date);
 
-SELECT * FROM rooms
-WHERE 
-(rooms.room_id NOT IN (SELECT room_id FROM bookings))
-	OR (rooms.room_id IN 
-	(SELECT room_id FROM bookings
-		WHERE rooms.[availability] = 1
-		AND '2024-05-15' NOT BETWEEN bookings.check_in_date AND bookings.check_out_date));
+SELECT *
+FROM rooms
+WHERE (rooms.room_id NOT IN (SELECT room_id FROM bookings))
+   OR (rooms.room_id IN
+       (SELECT room_id
+        FROM bookings
+        WHERE rooms.availability = 1
+          AND '2024-05-15' NOT BETWEEN bookings.check_in_date AND bookings.check_out_date));
