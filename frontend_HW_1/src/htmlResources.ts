7660acd223e6843471dcdd3c2fc49a1a89b36805
Андрей Пряшников <prandruha2003@yandex.ru>
2024-05-24 20:16:04 +0300

    Optimizations by comments

--- frontend_HW_1/src/htmlResources.ts
+++ frontend_HW_1/src/htmlResources.ts
@@ -3,13 +3,15 @@ import fs from 'fs';
 
 export const htmlResources = (filePath: string) => {
   const htmlRoot = parse(fs.readFileSync(filePath, 'utf8'));
-  const hrefs = new Set<string>();
 
-  htmlRoot.querySelectorAll('link[href], script[src], img[src], a[href]').map((element) => {
-    const attribute = element.getAttribute('href') || element.getAttribute('src');
-    if (attribute) {
-      hrefs.add(attribute);
-    }
-  });
+  const hrefs = htmlRoot.querySelectorAll('link[href], script[src], img[src], a[href]')
+    .reduce((set, element) => {
+      const attribute = element.getAttribute('href') || element.getAttribute('src');
+      if (attribute) {
+        set.add(attribute);
+      }
+      return set;
+    }, new Set<string>())
+
   return Array.from(hrefs);
 };
--- frontend_HW_1/src/jsonDiff.ts
+++ frontend_HW_1/src/jsonDiff.ts
@@ -1,74 +1,58 @@
 import fs from 'fs';
 
-type JsonValue = string | number | boolean | undefined | JsonObject;
-
-type JsonObject = {
-  [key: string]: JsonValue;
-};
+type JsonObject = Record<string, unknown>;
 
 type CompareResult = Record<string, {
   type: "new" | "delete" | "changed" | "unchanged",
-  oldValue?: JsonValue,
-  newValue?: JsonValue,
+  oldValue?: unknown,
+  newValue?: unknown,
   children?: CompareResult,
 }>;
 
-//Conversation from array into json object 
-const toJson = (tempObj: JsonValue): JsonValue => {
-  return (Array.isArray(tempObj))
-    ? tempObj.reduce((obj, item, index) => {
-      obj[index.toString()] = item;
-      return obj;
-    }, {})
-    : tempObj;
-}
+const isJsonObject = (value: unknown): value is JsonObject => {
+  return typeof value === 'object' && value !== null;
+};
 
 const compareObjects = (oldObject: JsonObject, newObject: JsonObject): CompareResult => {
   const keys = Array.from(new Set([...Object.keys(oldObject), ...Object.keys(newObject)]));
 
   return keys.reduce((compareResult, key) => {
-    const oldObj = key in oldObject && !(key in newObject);
-    const newObj = key in newObject && !(key in oldObject);
-    if (oldObj || newObj) {
+    const oldValue = oldObject[key];
+    const newValue = newObject[key];
+
+    if (oldValue === undefined || newValue === undefined) {
       return {
         ...compareResult,
-        [key]: newObj ? {
+        [key]: newValue ? {
           type: 'new',
-          newValue: newObject[key]
+          newValue
         } : {
           type: 'delete',
-          oldValue: oldObject[key]
+          oldValue
         }
       };
     }
 
-    const oldValue = toJson(oldObject[key]);
-    const newValue = toJson(newObject[key]);
-    if (oldValue !== undefined && newValue !== undefined) {
-      if (typeof oldValue === 'object' && typeof newValue === 'object' && oldValue !== null && newValue !== null) {
-        const childrenDifference = compareObjects(oldValue, newValue);
-        const hasChanged = Object.values(childrenDifference).some(child => child.type !== 'unchanged');
-        return {
-          ...compareResult,
-          [key]: {
-            type: !hasChanged ? 'unchanged' : 'changed',
-            children: childrenDifference
-          }
-        };
-      } else {
-        return {
-          ...compareResult,
-          [key]: {
-            type: oldValue === newValue ? 'unchanged' : 'changed',
-            oldValue,
-            newValue
-          }
-        };
-      }
+    if (isJsonObject(oldValue) && isJsonObject(newValue)) {
+      const childrenDifference = compareObjects(oldValue, newValue);
+      const hasChanged = Object.values(childrenDifference).some(child => child.type !== 'unchanged');
+      return {
+        ...compareResult,
+        [key]: {
+          type: !hasChanged ? 'unchanged' : 'changed',
+          children: childrenDifference
+        }
+      };
     }
+
     return {
-      ...compareResult
-    }
+      ...compareResult,
+      [key]: {
+        type: oldValue === newValue ? 'unchanged' : 'changed',
+        oldValue,
+        newValue
+      }
+    };
   }, {});
 };
 
